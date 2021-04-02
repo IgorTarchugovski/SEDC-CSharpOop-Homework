606d867233a997cb17fb76f85521bb98f3d69657
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-02 23:04:10 +0200

    class03-task01-homework-completed

--- Homework.CSharpOop.Class03/Homework.CSharpOop.Class03.Task01.SumOfEven/Program.cs
+++ Homework.CSharpOop.Class03/Homework.CSharpOop.Class03.Task01.SumOfEven/Program.cs
@@ -6,56 +6,76 @@ namespace Homework.CSharpOop.Class03.Task01.SumOfEven
     {
         static void Main(string[] args)
         {
-            #region Task 1 - "RealCalculator"
-
+            #region Task 1 - "SumOfEven"
             /*
-            Create new console application “RealCalculator” that takes two numbers from the input
-            and asks what operation would the user want to be done( +, - , * , / ). 
-            Then it returns the result.
+             Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print
+            the sum of the even numbers from the array:
             Test Data:
-            Enter the First number: 10
-            Enter the Second number: 15
-            Enter the Operation: +
+                Enter integer no.1: 4
+                Enter integer no.2: 3
+                Enter integer no.3: 7
+                Enter integer no.4: 3
+                Enter integer no.5: 2
+                Enter integer no.6: 8
             Expected Output:
-            The result is: 25
-            */
+                The result is: 14
+                
+             */
 
             #endregion
 
-            Console.WriteLine("Enter first number: ");
+            Console.WriteLine("\"SUM OF EVEN NUMBERS\"");
+
+            int[] arr = new int[6];
+
+            Console.Write("Enter integer no.1: ");
             string userInput1 = Console.ReadLine();
-            int num1 = int.Parse(userInput1);
+            int no1 = int.Parse(userInput1);
+            arr[0] = no1;
 
-            Console.WriteLine("Enter second number: ");
+            Console.Write("Enter integer no.2: ");
             string userInput2 = Console.ReadLine();
-            int num2 = int.Parse(userInput2);
+            int no2 = int.Parse(userInput2);
+            arr[1] = no2;
 
-            Console.WriteLine("Enter the operation: (+, -, *, /)");
-            string operation = Console.ReadLine();
+            Console.Write("Enter integer no.3: ");
+            string userInput3 = Console.ReadLine();
+            int no3 = int.Parse(userInput3);
+            arr[2] = no3;
 
+            Console.Write("Enter integer no.4: ");
+            string userInput4 = Console.ReadLine();
+            int no4 = int.Parse(userInput4);
+            arr[3] = no4;
 
-            if (operation == "+")
+            Console.Write("Enter integer no.5: ");
+            string userInput5 = Console.ReadLine();
+            int no5 = int.Parse(userInput5);
+            arr[4] = no5;
+
+            Console.Write("Enter integer no.6: ");
+            string userInput6 = Console.ReadLine();
+            int no6 = int.Parse(userInput6);
+            arr[5] = no6;
+
+            int sum = 0;
+
+            foreach (int num in arr)
             {
-                int sum = num1 + num2;
-                Console.WriteLine(num1 + " + " + num2 + " = " + sum);
-            }
-            else if (operation == "-")
-            {
-                int sub = num1 - num2;
-                Console.WriteLine(num1 + " - " + num2 + " = " + sub);
-            }
-            else if (operation == "*")
-            {
-                int multiplication = num1 * num2;
-                Console.WriteLine(num1 + " * " + num2 + " = " + multiplication);
-            }
-            else if (operation == "/")
-            {
-                int division = num1 / num2;
-                Console.WriteLine(num1 + " / " + num2 + " = " + division);
+                if (num % 2 == 0)
+                {
+                    sum = sum + num;
+                }
             }
 
-            Console.ReadLine();
+            if (sum % 2 != 0 || sum == 0)
+            {
+                Console.WriteLine("You did not enter an even number!");
+            }
+
+            Console.WriteLine("The sum of the even numbers from the array: " + sum);
+
+            Console.Read();
         }
     }
 }
