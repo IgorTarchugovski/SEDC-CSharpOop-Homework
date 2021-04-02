d0134f9f7987e048fa74d99fd559a2840c745f87
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-02 23:01:40 +0200

    class02-task03-homework-completed

---
+++ Homework.CSharpOop.Class02/Homework.CSharpOop.Class02.Task03.SwapNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Class02.Task03.SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 3 - "SwapNumbers"
+            /*
+            Create new console application “SwapNumbers”. 
+            Input 2 numbers from the console and then swap the values of the variables so that the first 
+            variable has the second value and the second variable the first value. Please find example below:
+            Test Data:
+            Input the First Number: 5
+            Input the Second Number: 8
+            Expected Output:
+            After Swapping:
+            First Number: 8
+            Second Number: 5
+             */
+
+            #endregion
+
+            Console.Write("Input the First Number: ");
+            string userInput1 = Console.ReadLine();
+            int num1 = int.Parse(userInput1);
+
+            Console.Write("Input the Second Number: ");
+            string userInput2 = Console.ReadLine();
+            int num2 = int.Parse(userInput2);
+
+            int temp;
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("\nAfter Swapping:");
+            Console.WriteLine("First Number: " + num1);
+            Console.WriteLine("Second Number: " + num2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework.CSharpOop.Class03/Homework.CSharpOop.Class03.Task01.SumOfEven/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Class03.Task01.SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1 - "RealCalculator"
+
+            /*
+            Create new console application “RealCalculator” that takes two numbers from the input
+            and asks what operation would the user want to be done( +, - , * , / ). 
+            Then it returns the result.
+            Test Data:
+            Enter the First number: 10
+            Enter the Second number: 15
+            Enter the Operation: +
+            Expected Output:
+            The result is: 25
+            */
+
+            #endregion
+
+            Console.WriteLine("Enter first number: ");
+            string userInput1 = Console.ReadLine();
+            int num1 = int.Parse(userInput1);
+
+            Console.WriteLine("Enter second number: ");
+            string userInput2 = Console.ReadLine();
+            int num2 = int.Parse(userInput2);
+
+            Console.WriteLine("Enter the operation: (+, -, *, /)");
+            string operation = Console.ReadLine();
+
+
+            if (operation == "+")
+            {
+                int sum = num1 + num2;
+                Console.WriteLine(num1 + " + " + num2 + " = " + sum);
+            }
+            else if (operation == "-")
+            {
+                int sub = num1 - num2;
+                Console.WriteLine(num1 + " - " + num2 + " = " + sub);
+            }
+            else if (operation == "*")
+            {
+                int multiplication = num1 * num2;
+                Console.WriteLine(num1 + " * " + num2 + " = " + multiplication);
+            }
+            else if (operation == "/")
+            {
+                int division = num1 / num2;
+                Console.WriteLine(num1 + " / " + num2 + " = " + division);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
