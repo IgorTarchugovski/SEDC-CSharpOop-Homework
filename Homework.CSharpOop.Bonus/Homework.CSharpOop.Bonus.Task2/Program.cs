f087d21dc27928934ffd703f3cee2a4bbea1f812
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-05 20:23:31 +0200

    bonus-task2-completed

---
+++ Homework.CSharpOop.Bonus/Homework.CSharpOop.Bonus.Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Bonus.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task2
+            /*
+             Task 2:
+             Write a program that will print out all numbers in the range 1-N (N is input from keyboard, N>5) that divide 2 and 3.
+            */
+            #endregion
+
+            Console.Write("Enter a mumber: ");
+            string userInput = Console.ReadLine();
+            bool isValid = Int32.TryParse(userInput, out int n);
+
+            if (!isValid || n <= 5)
+            {
+                Console.WriteLine("Please enter a number bigger than 5");
+            }
+            else
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    if (i % 2 == 0 && i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
