ea52bd752a3780f68296c04924a253d471562dc0
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-05 22:41:10 +0200

    bonus-task7-completed

---
+++ Homework.CSharpOop.Bonus/Homework.CSharpOop.Bonus.Task7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Bonus.Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task7
+            /*
+                Task 7:
+                Write a program that will reverse the array {“a”,”b”,”c”,”d”} so that the output is {“d”,“c”,“b”,“a”}.
+                - Do NOT use reverse array method :) 
+            */
+            #endregion
+
+            char[] arr = { 'a', 'b', 'c', 'd' };
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
