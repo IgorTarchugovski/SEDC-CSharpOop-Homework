1c7eac8b9c35a177bb1a18ce2eaf23a8a3e28a89
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-05 20:37:56 +0200

    bonus-task3-completed

---
+++ Homework.CSharpOop.Bonus/Homework.CSharpOop.Bonus.Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Bonus.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task3
+            /*
+                Task 3:
+                Write a program, which will be calculating the product of the digits of an input number. 
+            */
+            #endregion
+
+            Console.Write("Enter a number to return product: ");
+            string userInput = Console.ReadLine();
+            bool isValid = Int64.TryParse(userInput, out long n);
+
+            if (!isValid)
+            {
+                Console.WriteLine("Please enter a number");
+            }
+            else
+            {
+                long product = 1;
+
+                while (n != 0)
+                {
+                    product = product * (n % 10);
+                    n = n / 10;
+                }
+
+                Console.WriteLine(product);
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
