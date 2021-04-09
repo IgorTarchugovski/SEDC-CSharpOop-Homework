198352db85a14fa5539e6766e6891aadace6ac04
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-09 12:24:39 +0200

    bonus

---
+++ Homework.CSharpOop.Bonus/Homework.CSharpOop.Bonus.Task10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Bonus.Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task10
+            /*
+                Task 10.
+                Write a program that will find out all numbers in the range [1 : 1000] that divide the product composed of their digits.
+             */
+            #endregion
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
