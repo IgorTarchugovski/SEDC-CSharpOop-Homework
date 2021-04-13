e2d53c68d4be6f01231f9f825deddeb9c1f88b9a
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-13 13:40:47 +0200

    class04-task02-completed

--- Homework.CSharpOop.Class04/Homework.CSharpOop.Class04.Task2.RemoveExtraSpaceChar/Program.cs
+++ Homework.CSharpOop.Class04/Homework.CSharpOop.Class04.Task2.RemoveExtraSpaceChar/Program.cs
@@ -17,26 +17,31 @@ namespace Homework.CSharpOop.Class04.Task2.RemoveExtraSpaceChar
              */
             #endregion
 
-            string str = "The    best  Lorem  Ipsum        Generator in all the  sea!   Heave this   scurvy copyfiller fer yar         next   adventure  and cajol yar clients   into walking the plank with ev'ry layout!    Configure       above, then get yer pirate ipsum...own the high seas,    argh!";
+            string str1 = "The    best  Lorem  Ipsum        Generator in all the  sea!   Heave this   scurvy copyfiller fer yar         next   adventure  and cajol yar clients   into walking the plank with ev'ry layout!    Configure       above, then get yer pirate ipsum...own the high seas,    argh!";
 
-            string[] trimedArr = { };
-            string trimedStr;
-
-            string[] splited = str.Split(' ');
-            foreach (string word in splited)
+            static void RemoveExtraSpace(string str)
             {
-                if (word.Length != 0)
+                string[] trimedArr = { };
+                string trimedStr;
+
+                string[] splited = str.Split(' ');
+                foreach (string word in splited)
                 {
-                    Array.Resize(ref trimedArr, trimedArr.Length + 1);
-                    trimedArr[trimedArr.Length - 1] = word;                  
+                    if (word.Length != 0)
+                    {
+                        Array.Resize(ref trimedArr, trimedArr.Length + 1);
+                        trimedArr[trimedArr.Length - 1] = word;
+                    }
+                    //Console.WriteLine($"{word.Length} {word}");
                 }
-                //Console.WriteLine($"{word.Length} {word}");
+                trimedStr = string.Join(" ", trimedArr);
+
+                Console.WriteLine(trimedStr);
             }
-            trimedStr = string.Join(" ", trimedArr);
-
-            Console.WriteLine(trimedStr);
 
 
+
+            RemoveExtraSpace(str1);
 
             Console.ReadLine();
         }
