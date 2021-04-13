9242c1265f43f84798f9df7e39e941f2c81cb299
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-13 12:51:52 +0200

    class04-task02-completed

--- Homework.CSharpOop.Class04/Homework.CSharpOop.Class04.Task2.RemoveExtraSpaceChar/Program.cs
+++ Homework.CSharpOop.Class04/Homework.CSharpOop.Class04.Task2.RemoveExtraSpaceChar/Program.cs
@@ -6,7 +6,39 @@ namespace Homework.CSharpOop.Class04.Task2.RemoveExtraSpaceChar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Task 2
+            /*
+            Task 2
+            Create a new method that will remove extra space characters
+            Call the method with the following text: 
+            The    best  Lorem  Ipsum        Generator in all the  sea!   Heave this   scurvy copyfiller fer yar         
+            next   adventure  and cajol yar clients   into walking the plank with ev'ry layout!    Configure       
+            above, then get yer pirate ipsum...own the high seas,    argh!
+             */
+            #endregion
+
+            string str = "The    best  Lorem  Ipsum        Generator in all the  sea!   Heave this   scurvy copyfiller fer yar         next   adventure  and cajol yar clients   into walking the plank with ev'ry layout!    Configure       above, then get yer pirate ipsum...own the high seas,    argh!";
+
+            string[] trimedArr = { };
+            string trimedStr;
+
+            string[] splited = str.Split(' ');
+            foreach (string word in splited)
+            {
+                if (word.Length != 0)
+                {
+                    Array.Resize(ref trimedArr, trimedArr.Length + 1);
+                    trimedArr[trimedArr.Length - 1] = word;                  
+                }
+                //Console.WriteLine($"{word.Length} {word}");
+            }
+            trimedStr = string.Join(" ", trimedArr);
+
+            Console.WriteLine(trimedStr);
+
+
+
+            Console.ReadLine();
         }
     }
 }
