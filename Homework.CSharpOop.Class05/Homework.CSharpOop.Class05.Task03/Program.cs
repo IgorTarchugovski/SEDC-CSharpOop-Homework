1a4d83a3d14ce9ab9409245a6518cc1016ccbcca
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-23 13:41:55 +0200

    class05-task03-completed

--- Homework.CSharpOop.Class05/Homework.CSharpOop.Class05.Task03/Program.cs
+++ Homework.CSharpOop.Class05/Homework.CSharpOop.Class05.Task03/Program.cs
@@ -66,56 +66,6 @@ namespace Homework.CSharpOop.Class05.Task03
             }
 
 
-
-
-
-
-
-
-
-
-
-
-            /*           Console.Write("Enter length of array: ");
-                       string arrLengthInput = Console.ReadLine();
-                       bool isValid = int.TryParse(arrLengthInput, out int n);
-
-                       if (!isValid)
-                       {
-                           Console.WriteLine("Please enter a valid length numer!");
-                       }
-
-                       int[] arr = new int[n];
-                       for (int i = 0; i < n; i++)
-                       {
-                           Console.Write($"No.{i+1}: ");
-                           bool isVlaidNum = int.TryParse(Console.ReadLine(), out int num);
-                           if (!isVlaidNum)
-                           {
-                               Console.WriteLine("Please enter a valid number!");
-                               Console.Beep();
-                           }
-                           arr[i] = num;               
-                       }
-
-                       Console.WriteLine("=========================");
-                       Console.WriteLine($"Greatest value in array is: {GreatestValue(arr)}");
-
-
-                       // Function
-                       static int GreatestValue(int[] numArray)
-                       {
-                           int grtVal = 0;
-                           for (int i = 0; i < numArray.Length; i++)
-                           {
-                               if (numArray[i] > grtVal)
-                               {
-                                   grtVal = numArray[i];
-                               }
-                           }
-                           return grtVal;
-                       }
-           */
             Console.ReadLine();
         }
     }
