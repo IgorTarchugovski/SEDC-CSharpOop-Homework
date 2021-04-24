0bcb6f26b4f7b1d5b3dc88cf9c9e30a917094ef9
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-25 01:54:31 +0200

    class05-bonus-completed

---
+++ Homework.CSharpOop.Class05/Homework.CSharpOop.Class05.Bonus/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Class05.Bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Bonus Task
+            /*
+            Make a method called AgeCalculator
+            The method will have one input parameter, your birthday date
+            The method should return your age
+            Show the age of a user after he inputs a date
+            Note: take into consideration if the birthday is today, after or before today
+            */
+            #endregion
+
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.Write("Enter your Birth date (mm.dd.yyyy): ");
+                isValid = DateTime.TryParse(Console.ReadLine(), out DateTime userInput);
+                if (!isValid)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please enter a valid date format! (mm.dd.yyyy), (mm-dd-yyyy), (mm/dd/yyyy)");
+                    Console.ResetColor();
+                    Console.Beep();
+                }
+                else
+                {
+                    Console.WriteLine($"You are {AgeCalc(userInput)} years old.");
+                    if ((DateTime.Today.Month == userInput.Month) && (DateTime.Today.Day == userInput.Day))
+                    {
+                        Console.WriteLine("Happy Birthday!");
+                    }
+                }
+            }
+
+
+            static int AgeCalc(DateTime inputDate)
+            {
+                DateTime currentDate = DateTime.Now;
+                int currentDay = currentDate.Day;
+                int currentMonth = currentDate.Month;
+                int currentYear = currentDate.Year;
+
+                int inputDay = inputDate.Day;
+                int inputMonth = inputDate.Month;
+                int inputYear = inputDate.Year;
+
+                int age = currentYear - inputYear;
+
+                if (inputMonth > currentMonth)
+                {
+                    age -= 1;
+                }
+                if (inputMonth == currentMonth && inputDay > currentDay)
+                {
+                    age -= 1;
+                }
+                return age;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
