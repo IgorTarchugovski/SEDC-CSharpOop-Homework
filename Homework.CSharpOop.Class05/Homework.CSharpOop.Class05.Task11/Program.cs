b0ed88342998abc1e8e0e410cabd20813c78ae3e
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-25 00:24:35 +0200

    class05-task11-completed

---
+++ Homework.CSharpOop.Class05/Homework.CSharpOop.Class05.Task11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Class05.Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 11
+            /*
+             Task 11:
+             Create a function that takes an array and returns the difference between the biggest and smallest numbers.
+             [10, 4, 1, 4, -10, -50, 32, 21] -> 82
+            */
+            #endregion
+
+            int[] inputArr = new int[] { 10, 4, 1, 4, -10, -50, 32, 21 };
+            Console.WriteLine(DifferenceCalc(inputArr));
+
+            static int DifferenceCalc(int[] arr)
+            {
+                int biggestNum = arr[0];
+                int smallestNum = arr[0];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > biggestNum)
+                    {
+                        biggestNum = arr[i];                       
+                    }
+                    if (arr[i] < smallestNum)
+                    {
+                        smallestNum = arr[i];
+                    }
+                }
+                return biggestNum - smallestNum;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
