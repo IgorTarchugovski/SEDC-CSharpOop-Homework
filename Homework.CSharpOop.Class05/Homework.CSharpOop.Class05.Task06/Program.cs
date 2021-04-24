d1687035f932005b6814e8f123da236967283d46
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-24 18:52:36 +0200

    class05-task06-completed

---
+++ Homework.CSharpOop.Class05/Homework.CSharpOop.Class05.Task06/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Class05.Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 6
+            /*
+            Task 6:
+            Create a function to check whether a character is an alphabet or not and if so, go to check for the case (upper or lower). 
+            */
+            #endregion
+
+            Console.WriteLine("Enter a character to check if it's an alphabet: ");
+            char ch;
+            ch = Console.ReadLine()[0];
+
+            IsItAnAlphabet(ch);
+            
+
+            static void IsItAnAlphabet(char character)
+            {
+                if (char.IsLetter(character))
+                {
+                    if (char.IsUpper(character))
+                    {
+                        Console.WriteLine($"The character \"{character}\" is an alphabet and is uppercase.");                        
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The character \"{character}\" is an alphabet and is lowercase.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"The character \"{character}\" is NOT an alphabet.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
