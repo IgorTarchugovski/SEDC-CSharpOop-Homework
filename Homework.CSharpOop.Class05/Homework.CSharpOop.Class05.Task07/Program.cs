e48982b20e62c0740d98ba645f50b861432f432e
Igor Tarchugovski <Igor.t93@outlook.com>
2021-04-24 21:07:51 +0200

    class05-task07-completed

---
+++ Homework.CSharpOop.Class05/Homework.CSharpOop.Class05.Task07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework.CSharpOop.Class05.Task07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 7
+            /*
+                Task 7:
+                Create a function to find the number of times a substring appears in a given string.
+            */
+            #endregion
+            Console.WriteLine("Enter a string: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Enter a substring: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine($"The substring \"{word}\" appears \"{CheckOccurrences(text, word)}\" times in the string \"{text}\".");
+
+            static int CheckOccurrences(string str, string subStr)
+            {
+                int count = 0;
+                int a = 0;
+                while ((a = str.IndexOf(subStr, a)) != -1)
+                {
+                    a += subStr.Length;
+                    count++;
+                }
+                return count;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
